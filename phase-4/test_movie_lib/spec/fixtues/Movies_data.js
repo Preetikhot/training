21d0edb44009d7712570be7e5ac790e9532ef7c4
Preeti Khot <preetimk99@gmail.com>
2022-01-24 18:14:09 +0530

    first commit

---
+++ phase-4/test_movie_lib/spec/fixtues/Movies_data.js
@@ -0,0 +1,32 @@
+
+
+//restorents information are stored in this file using Fixtures
+var FIXTURES = window.FIXTURES || {};
+
+FIXTURES.movies ={
+
+    movie1:{
+        name:"monalisa",
+        rating:"5.0",
+        genres:"comedy",
+        comment:"very Good"
+    },
+    movie2:{
+        name:"maina",
+        rating:"4.6",
+        genres:"adventure",
+        comment:"excelent movie"
+    },
+    movie3:{
+        name:"guru shishya",
+        rating:"4.3",
+        genres:"action",
+        comment:"Good"
+    },
+    movie4:{
+        name:"kanasugar",
+        rating:"5.0",
+        genres:"horror",
+        comment:"Good"
+    },
+}
---
+++ phase-4/test_movie_lib/src/MovieModel.js
@@ -0,0 +1,59 @@
+var app = window.app || {};
+
+app.Models = app.Models || {};
+
+app.Models.Movie = Backbone.Model.extend({
+    
+
+    defaults: function () {
+
+        return {
+            name: 'swami',
+            rating: '4.1',
+            genre:'action',
+            comment:'Good'
+        };
+    },
+    
+    names:function(){
+        return this.get('name')
+    },
+
+    rating:function(){
+        return this.get('rating')
+    },
+
+    ishorrormovie:function(){
+        if(this.get('genres')=='horror'){
+            return true
+        }
+        else{
+            return false
+        }
+    },
+    iscomedymovie:function(){
+        if(this.get('genres')=='comedy'){
+            return true
+        }
+        else{
+            return false
+        }
+    },
+    isactionmovie:function(){
+        if(this.get('genres')=='action'){
+            return true
+        }
+        else{
+            return false
+        }
+    },
+    isadventuremovie:function(){
+        if(this.get('genres')=='adventure'){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+});
